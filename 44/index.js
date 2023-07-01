dd39ce14004d50cf5b15c0f676d5feb84c750278
Kamal Singh Rawat <rawatkamal1542002@gmail.com>
2023-07-02 00:21:58 +0530

    web

---
+++ 44/index.js
@@ -0,0 +1,20 @@
+console.log('chaliye bhai');
+
+
+
+//factory function
+function createreactangle(len,bre){
+    return reactangle = {
+        length: len,
+        breadth: bre,
+    
+    
+    draw: function() {
+        console.log('draw');
+    }
+
+    
+
+    };
+//return reactangle;        
+}
---
+++ 45/index.js
@@ -0,0 +1,8 @@
+let number = [1,3,5,7];
+console.log(number);
+number.push(9);
+console.log(number);
+number.unshift(8);
+console.log(number);
+number.splice(2,0, "mid");
+console.log(number);
