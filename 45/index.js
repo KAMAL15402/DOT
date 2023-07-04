ff8f44f5727c938f0f61b2e965c252491d5789da
Kamal Singh Rawat <rawatkamal1542002@gmail.com>
2023-07-04 12:06:00 +0530

    46

--- 45/index.js
+++ 45/index.js
@@ -1,8 +1,55 @@
-let number = [1,3,5,7];
-console.log(number);
-number.push(9);
-console.log(number);
-number.unshift(8);
-console.log(number);
-number.splice(2,0, "mid");
-console.log(number);
+// let number = [1,3,5,7];
+// console.log(number);
+// number.push(9);
+// console.log(number);
+// number.unshift(8);
+// console.log(number);
+// number.splice(2,0, "mid");
+// console.log(number);
+// let courses = [
+//     {no:1, cname: 'biology'},
+//     {no:2, cname:'maths'}
+// ];
+
+// let course = courses.find(function(course){
+//     return course.cname === 'maths';
+// })
+
+// let course = courses.find(course=> course.cname === 'biology');  
+// console.log(course);
+
+let first = [1,2];
+let second = [3,4];
+let com = [...first ,...second]; // spread operator
+//console.log(com);
+
+
+let arr = [1,2,3,4,5,6,7,8];
+
+//for of loop
+// for(let value of arr){
+//     console.log(value);
+// }
+
+// arr.forEach(function(number){
+//     console.log(number)
+// });
+
+
+let numbers = [3, 1, 4, 1, 5, 9, 2, 6, 5];
+numbers.sort(function(a, b) {
+    return a - b;
+});
+console.log(numbers); 
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 46/index.js
@@ -0,0 +1,44 @@
+//hoisting is the process of moving function automatically to the top
+
+// run();
+
+// function run(){
+//     console.log('run as fast');
+// }
+
+// function add(a,b){
+//     return a+b;
+// }
+
+// function add(){
+//  let total =0;
+//  for (let i of arguments) {
+//     total += i;
+// }
+// return total;
+// }
+
+//  console.log(add(1,5,3));
+
+
+
+                  //getter setter
+// let  person = {
+//     fname: 'John',
+//     lname:'Doe',
+//     get fullname(){
+//         return `${person.fname}  ${person.lname}`;
+//     },
+
+//     set fullname(value){
+//         let parts = value.split(' ');
+//         this.fname = parts[0];
+//         this.lname=parts[1];
+//     }
+
+
+
+// };
+
+                            //try catch
+                            //scope
